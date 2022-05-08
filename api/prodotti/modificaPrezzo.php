ca29c7365c7c68d22a06ba82ed4f4352fd873ced
Riccumeni <riccumenialessandro01@gmail.com>
2022-05-08 11:54:52 +0200

    aggiunti file ferrone

---
+++ api/prodotti/modificaPrezzo.php
@@ -0,0 +1,30 @@
+<?php
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Methods: POST");
+
+	//includiamo configurazioni e oggetti
+	include_once 'db.php';
+
+	//Connessione db
+	$database = new Database();
+	$db = $database->getConnection();
+
+	
+	$data = json_decode(file_get_contents("php://input"));
+	if(!empty($data->codice) && !empty($data->prezzo)){
+
+        $query = "UPDATE prodotto SET prezzo='$data->prezzo' WHERE id='$data->codice'";
+		$q = $db->prepare($query);
+        if($q->execute()){
+            $r = array("Success"=>"true","Message" => "Prezzo modificato");
+            echo json_encode($r);
+        }else{
+            $r = array("Success"=>"false","Message" => "Prezzo non modificato");
+            echo json_encode($r);
+        }
+
+	}else{
+		$r = array("Success"=>"false","Message" => "Campi mancanti");
+		echo json_encode($r);
+	}
+?>

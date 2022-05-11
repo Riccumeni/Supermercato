02dd69c8829943cc8745054176595674791088f7
Riccumeni <riccumenialessandro01@gmail.com>
2022-05-11 16:20:20 +0200

    Aggiunte funzionalità password
    - Modifica password da loggato
    - Recupero password con email

---
+++ api/accesso/password/modifica.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Methods: POST");
+
+$data = json_decode(file_get_contents("php://input"));
+$id = $data->id;
+$newpass = $data->nuova_password;
+
+$server = "localhost";
+$username = "root";
+$pass = "";
+$db = "Supermercato";
+
+$conn = new mysqli($server, $username, $pass, $db);
+
+if($conn && (!empty($id) && !empty($newpass))){ 
+
+    $pass_hashed = password_hash($newpass, PASSWORD_DEFAULT);
+
+    $sql = "update utente set password = '$pass_hashed' where id='$id'";
+    $conn->query($sql);
+
+    if($conn->affected_rows > 0){
+        echo json_encode(array("success" => true, "message" => "Password modificata con successo!"));
+    }else{
+        echo json_encode(array("success" => false, "message" => "Errore con la modifica!"));
+    } 
+}else{
+    echo json_encode(array("success" => false, "message" => "Errore con la connessione al database o campi mancanti!"));
+}
+?>
---
+++ api/accesso/password/recupera.php
@@ -0,0 +1,40 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Methods: POST");
+
+$data = json_decode(file_get_contents("php://input"));
+$email = $data->email;
+
+$server = "localhost";
+$username = "root";
+$pass = "";
+$db = "Supermercato";
+
+$conn = new mysqli($server, $username, $pass, $db);
+
+if($conn && !empty($email)){
+    $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $newpass = array(); 
+    $combLen = strlen($comb) - 1; 
+
+    for ($i = 0; $i < 8; $i++) { 
+        $n = rand(0, $combLen);
+        $newpass[] = $comb[$n];
+    }
+    $newpass = implode($newpass); 
+
+    $pass_hashed = password_hash($newpass, PASSWORD_DEFAULT);
+
+    $sql = "update utente set password = '$pass_hashed' where email='$email'";
+    $conn->query($sql);
+
+    if($conn->affected_rows > 0){
+        mail($email, "Recupero password", "E' stata modificata la password in -> $newpass \nRicordarsi di modificarla il prima possibile");
+        echo json_encode(array("success" => true, "message" => "mail inviata!"));
+    }else{
+        echo json_encode(array("success" => false, "message" => "Errore!"));
+    } 
+}else{
+    echo json_encode(array("success" => false, "message" => "Errore con la connessione al database o campi mancanti!"));
+}
+?>
